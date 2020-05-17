e66431c7deda80f1ba0268845fe7b619bf9b0be0
Connor Young <connorkyoung@outlook.com>
2020-05-17 11:36:57 -0400

    Init Boilerplate

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path"); // Module for lazy url concat
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const app = express(); // Instantiate express to app var
+const port = process.env.PORT || 4242;
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "index",
+  });
+});
+
+const server = app.listen(4242, () => {
+  console.log(`app is running on port ${server.address().port} ....`);
+});
