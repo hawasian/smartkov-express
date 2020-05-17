99de17c0ab137f23f7f67ae3847dbf8daa0219e3
Connor Young <connorkyoung@outlook.com>
2020-05-17 12:31:05 -0400

    .gitignore init

--- app.js
+++ app.js
@@ -4,17 +4,29 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
 const app = express(); // Instantiate express to app var
 const port = process.env.PORT || 4242;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: "Shh, its a secret!" }));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "index",
+    uname: req.session.uname || "World",
   });
 });
 
+app.post("/submit_user", (req, res) => {
+  req.session.uname = req.body.uname;
+  res.redirect("/");
+});
 const server = app.listen(port, () => {
   console.log(`app is running on port ${server.address().port} ....`);
 });
